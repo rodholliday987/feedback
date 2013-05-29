e95101ddbba47e4ee3ecae15d440e83b359e3823
rodholliday987 <rodrigo@marylin.(none)>
2013-05-29 10:41:56 -0500

    cambios en el home

--- application/views/inicio/inicio_v.php
+++ application/views/inicio/inicio_v.php
@@ -1,5 +1,3 @@
 <div class="row">
-    <div class="twelve columns ">Rodrigo eres el amor de mi vida, espero lo sepas. :D
-Si puedes ver esto quiere decir que ya todo esta bien.
-Te amo al rato nos vemos para bailar. :D</div>
+    <div class="twelve columns "></div>
 </div>
